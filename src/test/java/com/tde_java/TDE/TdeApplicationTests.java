1227b99c73aee0af3bc6bb687fb93c762e4f7fdc
Joao Treto <joaoatreto@gmail.com>
2024-05-21 20:15:34 -0300

    First commit

---
+++ src/test/java/com/tde_java/TDE/TdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tde_java.TDE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
